d258814898f94d1457a4bbc1596f50d34960bdf4
Ron Kurr <kurr@kurron.org>
2015-10-10 17:15:44 -0400

    Not sure

--- syncthing.sh
+++ syncthing.sh
@@ -4,7 +4,7 @@ mkdir -p "$HOME/.config/syncthing"
 
 set -x
 
-docker run --detach --name syncthing --restart always --user "$(id -u):$(id -g)" --volume "$HOME:$HOME" --volume "$HOME/.config/syncthing:/home/user/.config/syncthing" --net host --log-driver syslog --log-opt syslog-tag="syncthing" tianon/syncthing "$@"
+docker run --detach --name syncthing --restart always --user "$(id -u):$(id -g)" --volume "$HOME:$HOME" --volume "$HOME/.config/syncthing:/home/user/.config/syncthing" --net host --log-driver syslog --log-opt syslog-tag="syncthing" --publish 8384:8384 tianon/syncthing "$@"
 
 #timeout 10s docker logs -f syncthing || true
  
