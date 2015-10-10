506c95c9e5b75ebb6d16050e19ae93097f84a089
Ron Kurr <kurr@kurron.org>
2015-10-10 20:47:41 +0000

    Send logs to syslog

--- syncthing.sh
+++ syncthing.sh
@@ -3,7 +3,8 @@
 mkdir -p "$HOME/.config/syncthing"
 
 set -x
-docker run --detach --name syncthing --restart always --user "$(id -u):$(id -g)" --volume "$HOME:$HOME" --volume "$HOME/.config/syncthing:/home/user/.config/syncthing" --net host tianon/syncthing "$@"
+
+docker run --detach --name syncthing --restart always --user "$(id -u):$(id -g)" --volume "$HOME:$HOME" --volume "$HOME/.config/syncthing:/home/user/.config/syncthing" --net host --log-driver syslog --log-opt syslog-tag="syncthing" tianon/syncthing "$@"
 
 timeout 10s docker logs -f syncthing || true
  
