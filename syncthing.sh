fcbca7a9b30197a005f565cc26be4d49bd9cb4b1
Ron Kurr <kurr@kurron.org>
2015-10-10 16:54:40 -0400

    Commented out log tailing

--- syncthing.sh
+++ syncthing.sh
@@ -6,5 +6,5 @@ set -x
 
 docker run --detach --name syncthing --restart always --user "$(id -u):$(id -g)" --volume "$HOME:$HOME" --volume "$HOME/.config/syncthing:/home/user/.config/syncthing" --net host --log-driver syslog --log-opt syslog-tag="syncthing" tianon/syncthing "$@"
 
-timeout 10s docker logs -f syncthing || true
+#timeout 10s docker logs -f syncthing || true
  
