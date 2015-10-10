01d17f488e4a5792ed7150b02049c83d10fee5b2
Ron Kurr <kurr@kurron.org>
2015-10-09 23:01:52 -0400

    Switched to using scripts to run OpenVPN

--- initialize-openvpn.sh
+++ initialize-openvpn.sh
@@ -5,5 +5,5 @@ OVPN_DATA=openvpn-data
 docker run --name $OVPN_DATA -v /etc/openvpn busybox
 docker run --volumes-from $OVPN_DATA --rm kylemanna/openvpn ovpn_genconfig -u udp://rkurr.duckdns.org
 docker run --volumes-from $OVPN_DATA --rm -it kylemanna/openvpn ovpn_initpki
-docker run --volumes-from $OVPN_DATA -d -p 1194:1194/udp --cap-add=NET_ADMIN -e DEBUG=1 kylemanna/openvpn
+docker run --volumes-from $OVPN_DATA --name openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN -e DEBUG=1 kylemanna/openvpn
 
